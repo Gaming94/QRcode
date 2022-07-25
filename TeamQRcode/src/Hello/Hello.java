08f194a0024a616048b99d273db9a976daec69d9
Gaming94 <sso01177@naver.com>
2022-07-25 17:56:28 +0900

    [웨 안데?]

---
+++ TeamQRcode/src/Hello/Hello.java
@@ -0,0 +1,11 @@
+package Hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Let me introduce my team member");
+		System.out.println("가민석 가민석 가민석 가민석 가민석");
+	}
+
+}
