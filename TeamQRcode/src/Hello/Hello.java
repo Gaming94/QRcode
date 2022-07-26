75dc88bbcfc0f67bd8d79a27048968f733e75326
Gaming94 <sso01177@naver.com>
2022-07-26 12:16:06 +0900

    [ServerTest]

--- TeamQRcode/src/Hello/Hello.java
+++ TeamQRcode/src/Hello/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Let me introduce my team member");
-		System.out.println("가민석 가민석 가민석 가민석 가민석");
+		System.out.println("가민석 가민석 가민석 이창재 가민석");
 	}
 
 }
---
+++ TeamQRcode/src/QRcode/Empty.java
@@ -0,0 +1,10 @@
+package QRcode;
+
+public class Empty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
