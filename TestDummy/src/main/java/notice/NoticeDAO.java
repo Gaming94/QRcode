5cee472f03de46904d9a6f2f16781ed4755268c9
EternalSW <best31219@naver.com>
2022-08-04 12:33:37 +0900

    공지사항

---
+++ TestDummy/src/main/java/notice/NoticeDAO.java
@@ -0,0 +1,5 @@
+package notice;
+
+public class NoticeDAO {
+
+}
---
+++ TestDummy/src/main/java/notice/NoticeVO.java
@@ -0,0 +1,54 @@
+package notice;
+
+import java.util.Date;
+
+public class NoticeVO {
+	private int no;
+	private String title;
+	private String content;
+	private Date regdates;
+	
+	public NoticeVO() {
+	}
+	
+	public NoticeVO(int no, String title, String content, Date regdates) {
+		super();
+		this.no = no;
+		this.title = title;
+		this.content = content;
+		this.regdates = regdates;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getRegdates() {
+		return regdates;
+	}
+
+	public void setRegdates(Date regdates) {
+		this.regdates = regdates;
+	}
+	
+}
